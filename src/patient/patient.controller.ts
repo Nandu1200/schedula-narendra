e9f7df38a7290b13e1a59d6914f1a0b8e4a49e43
Narendra Bhadouriya <narendrabhadou18@gmail.com>
2026-06-08 19:37:52 +0530

    Day 3: Implement doctor and patient onboarding

--- src/patient/patient.controller.ts
+++ src/patient/patient.controller.ts
@@ -1,18 +1,53 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Param,
+  Body,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { RolesGuard } from '../auth/roles.guard';
 import { Roles } from '../auth/roles.decorator';
 import { Role } from '../auth/roles.enum';
 
+import { PatientService } from './patient.service';
+import { CreatePatientDto } from './dto/create-patient.dto';
+
 @Controller('patient')
 export class PatientController {
+  constructor(private readonly patientService: PatientService) {}
+
+  @Post('profile')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.PATIENT)
+  createProfile(
+    @Body() body: CreatePatientDto,
+    @Req() req: any,
+  ) {
+    return this.patientService.createProfile({
+      ...body,
+      userId: req.user.userId,
+    });
+  }
+
   @Get('profile')
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.PATIENT)
   getProfile() {
-    return {
-      message: 'Patient Profile',
-    };
+    return this.patientService.getProfile();
+  }
+
+  @Patch('profile/:id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.PATIENT)
+  updateProfile(
+    @Param('id') id: string,
+    @Body() body: any,
+  ) {
+    return this.patientService.updateProfile(+id, body);
   }
 }
